4e8d42c995f23a14c36959419ecef5e4a9c79acb
Aliaksandr Polautsau <me@polautsau.com>
2025-10-24 14:48:24 +0200

    Initial Commit

---
+++ My Trails/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  My Trails
+//
+//  Created by Aliaksandr Polautsau on 24/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ My Trails/My_TrailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_TrailsApp.swift
+//  My Trails
+//
+//  Created by Aliaksandr Polautsau on 24/10/25.
+//
+
+import SwiftUI
+
+@main
+struct My_TrailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
